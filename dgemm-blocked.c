76330519b6ae1434ba92f3cfac973441eb0e17ec
shaoweiliu <shaoweiliu@shaoweilius-MacBook-Pro.local>
2020-01-25 23:05:55 -0800

    no buffer

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -15,7 +15,7 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 #define L1_BLOCK_SIZE 36
 #define L2_BLOCK_SIZE 104
 #define L3_BLOCK_SIZE 1144
-#define AVX_BLOCK_SIZE 4
+#define AVX_BLOCK_SIZE 8
 // #define BLOCK_SIZE 719
 #endif
 
@@ -33,11 +33,23 @@ static void do_block_l1 (int lda, int M_L1, int N_L1, int K_L1, double* A, doubl
       /* For each column j of B */
       for (int j = 0; j < N_L1; j+=AVX_BLOCK_SIZE)
       {
-          /* c: AVX_BLOCK_SIZE * 4 */
           register __m256d c00_c01_c02_c03 = _mm256_loadu_pd(C+i*lda+j);
+          register __m256d c04_c05_c06_c07 = _mm256_loadu_pd(C+i*lda+j+4);
           register __m256d c10_c11_c12_c13 = _mm256_loadu_pd(C+(i+1)*lda+j);
+          register __m256d c14_c15_c16_c17 = _mm256_loadu_pd(C+(i+1)*lda+j+4);
           register __m256d c20_c21_c22_c23 = _mm256_loadu_pd(C+(i+2)*lda+j);
+          register __m256d c24_c25_c26_c27 = _mm256_loadu_pd(C+(i+2)*lda+j+4);
           register __m256d c30_c31_c32_c33 = _mm256_loadu_pd(C+(i+3)*lda+j);
+          register __m256d c34_c35_c36_c37 = _mm256_loadu_pd(C+(i+3)*lda+j+4);
+          
+          register __m256d c40_c41_c42_c43 = _mm256_loadu_pd(C+(i+4)*lda+j);
+          register __m256d c44_c45_c46_c47 = _mm256_loadu_pd(C+(i+4)*lda+j+4);
+          register __m256d c50_c51_c52_c53 = _mm256_loadu_pd(C+(i+5)*lda+j);
+          register __m256d c54_c55_c56_c57 = _mm256_loadu_pd(C+(i+5)*lda+j+4);
+          register __m256d c60_c61_c62_c63 = _mm256_loadu_pd(C+(i+6)*lda+j);
+          register __m256d c64_c65_c66_c67 = _mm256_loadu_pd(C+(i+6)*lda+j+4);
+          register __m256d c70_c71_c72_c73 = _mm256_loadu_pd(C+(i+7)*lda+j);
+          register __m256d c74_c75_c76_c77 = _mm256_loadu_pd(C+(i+7)*lda+j+4);
           
           for (int k = 0; k < K_L1; k+=4)
            /*4 here 256/sizeof(double)/8=4 */
@@ -48,27 +60,66 @@ static void do_block_l1 (int lda, int M_L1, int N_L1, int K_L1, double* A, doubl
                   register __m256d a2x = _mm256_broadcast_sd(A+(i+2)*lda+k+kk);
                   register __m256d a3x = _mm256_broadcast_sd(A+(i+3)*lda+k+kk);
                   
-                  register __m256d b = _mm256_loadu_pd(B+(k+kk)*lda+j);
+                  register __m256d a4x = _mm256_broadcast_sd(A+(i+4)*lda+k+kk);
+                  register __m256d a5x = _mm256_broadcast_sd(A+(i+5)*lda+k+kk);
+                  register __m256d a6x = _mm256_broadcast_sd(A+(i+6)*lda+k+kk);
+                  register __m256d a7x = _mm256_broadcast_sd(A+(i+7)*lda+k+kk);
+                  
+                  register __m256d b0123 = _mm256_loadu_pd(B+(k+kk)*lda+j);
+                  register __m256d b4567 = _mm256_loadu_pd(B+(k+kk)*lda+j+4);
                   /* boundary padding */
-                  if (j+4>N_L1)
+                  if (j+AVX_BLOCK_SIZE>N_L1)
                   {
                       int e = N_L1 - j;
                       b[0] = b[0] * (e>0);
                       b[1] = b[1] * (e>1);
                       b[2] = b[2] * (e>2);
                       b[3] = b[3] * (e>3);
+                      b[4] = b[4] * (e>4);
+                      b[5] = b[5] * (e>5);
+                      b[6] = b[6] * (e>6);
+                      b[7] = b[7] * (e>7);
                   }
 
-                  c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, b, c00_c01_c02_c03);
-                  c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, b, c10_c11_c12_c13);
-                  c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, b, c20_c21_c22_c23);
-                  c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, b, c30_c31_c32_c33);
+                  c00_c01_c02_c03 = _mm256_fmadd_pd(a0x, b0123, c00_c01_c02_c03);
+                  c10_c11_c12_c13 = _mm256_fmadd_pd(a1x, b0123, c10_c11_c12_c13);
+                  c20_c21_c22_c23 = _mm256_fmadd_pd(a2x, b0123, c20_c21_c22_c23);
+                  c30_c31_c32_c33 = _mm256_fmadd_pd(a3x, b0123, c30_c31_c32_c33);
+                  
+                  c40_c41_c42_c43 = _mm256_fmadd_pd(a4x, b0123, c40_c41_c42_c43);
+                  c50_c51_c52_c53 = _mm256_fmadd_pd(a5x, b0123, c50_c51_c52_c53);
+                  c60_c61_c62_c63 = _mm256_fmadd_pd(a6x, b0123, c60_c61_c62_c63);
+                  c70_c71_c72_c73 = _mm256_fmadd_pd(a7x, b0123, c70_c71_c72_c73);
+                  
+                  c04_c05_c06_c07 = _mm256_fmadd_pd(a0x, b4567, c04_c05_c06_c07);
+                  c14_c15_c16_c17 = _mm256_fmadd_pd(a1x, b4567, c14_c15_c16_c17);
+                  c24_c25_c26_c27 = _mm256_fmadd_pd(a2x, b4567, c24_c25_c26_c27);
+                  c34_c35_c36_c37 = _mm256_fmadd_pd(a3x, b4567, c34_c35_c36_c37);
+                  
+                  c44_c45_c46_c47 = _mm256_fmadd_pd(a4x, b4567, c44_c45_c46_c47);
+                  c54_c55_c56_c57 = _mm256_fmadd_pd(a5x, b4567, c54_c55_c56_c57);
+                  c64_c65_c66_c67 = _mm256_fmadd_pd(a6x, b4567, c64_c65_c66_c67);
+                  c74_c75_c76_c77 = _mm256_fmadd_pd(a7x, b4567, c74_c75_c76_c77);
+                  
               }
 
           _mm256_storeu_pd(C+i*lda+j, c00_c01_c02_c03);
+          _mm256_storeu_pd(C+i*lda+j+4, c04_c05_c06_c07);
           _mm256_storeu_pd(C+(i+1)*lda+j, c10_c11_c12_c13);
+          _mm256_storeu_pd(C+(i+1)*lda+j+4, c14_c15_c16_c17);
           _mm256_storeu_pd(C+(i+2)*lda+j, c20_c21_c22_c23);
+          _mm256_storeu_pd(C+(i+2)*lda+j+4, c24_c25_c26_c27);
           _mm256_storeu_pd(C+(i+3)*lda+j, c30_c31_c32_c33);
+          _mm256_storeu_pd(C+(i+3)*lda+j+4, c34_c35_c36_c37);
+          
+          _mm256_storeu_pd(C+(i+4)*lda+j, c40_c41_c42_c43);
+          _mm256_storeu_pd(C+(i+4)*lda+j+4, c44_c45_c46_c47);
+          _mm256_storeu_pd(C+(i+5)*lda+j, c50_c51_c52_c53);
+          _mm256_storeu_pd(C+(i+5)*lda+j+4, c54_c55_c56_c57);
+          _mm256_storeu_pd(C+(i+6)*lda+j, c60_c61_c62_c63);
+          _mm256_storeu_pd(C+(i+6)*lda+j+4, c64_c65_c66_c67);
+          _mm256_storeu_pd(C+(i+7)*lda+j, c70_c71_c72_c73);
+          _mm256_storeu_pd(C+(i+7)*lda+j+4, c74_c75_c76_c77);
           
       }
 }
