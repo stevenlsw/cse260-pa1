5ad9b511c8c946a6604d7201082c86598413ed7d
shaoweiliu <shaoweiliu@shaoweilius-MacBook-Pro.local>
2020-01-29 00:56:10 -0800

    3 * 12 optimize

--- dgemm-blocked.c
+++ dgemm-blocked.c
@@ -133,9 +133,10 @@ void square_dgemm (int lda, double* restrict A, double* restrict B, double* rest
     /* Matrix padding and buffering */
     int  buffer_size = lda + AVX_BLOCK_SIZE_W - lda % AVX_BLOCK_SIZE_W;
     
-    double *buffer_A = (double*) _mm_malloc(buffer_size * buffer_size * sizeof(double), 2048);
-    double *buffer_B = (double*) _mm_malloc(buffer_size * buffer_size * sizeof(double), 2048);
-    double *buffer_C = (double*) _mm_malloc(buffer_size * buffer_size * sizeof(double), 2048);
+    double* buffer = (double*) _mm_malloc(3 * buffer_size * buffer_size * sizeof(double), 64*buffer_size);
+    double *buffer_A = buffer
+    double *buffer_B = buffer + buffer_size * buffer_size
+    double *buffer_C = buffer_B + buffer_size * buffer_size
    
     
     for (int i = 0; i < lda; ++i)
